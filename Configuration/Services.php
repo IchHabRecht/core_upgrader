0d48038fa2e283e5681169d7e7425d5c692d44ca
Lukas Niestroj <niestrojlukas@gmail.com>
2022-02-03 13:44:37 +0100

    replace Commands.php with Services.php

---
+++ Configuration/Services.php
@@ -0,0 +1,17 @@
+<?php
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return function (ContainerConfigurator $configurator) {
+    $services = $configurator->services();
+
+    if (class_exists(\Helhum\Typo3Console\Core\Booting\RunLevel::class)) {
+        $services->set(\IchHabRecht\Upgrader\Command\UpgradeCommand::class)
+            ->tag('console.command', [
+                'command' => 'coreupgrader:upgrade',
+                'runLevel' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE,
+                'schedulable' => false
+            ]);
+    }
+
+};
